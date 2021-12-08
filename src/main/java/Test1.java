465e7b87c337a0d230d529b1a4fbeb2ec63f278b
pratiksha <jadhav.pratiksha001@gmail.com>
2021-12-08 20:55:35 +0530

    commit 1

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,24 +1,6 @@
 public class Test1 {
-    public static void main(String args[]){
-       
-    }
-
-    public void a(){
-        //test
-        System.out.print("abc");
-        System.out.print("abc");
-       
-
-
+    public static void main(String args[]) {
 
     }
-    public void b(){
-    }
-//abcd
 
-    ///test
 }
-
-    public void c(){}
-}
-
